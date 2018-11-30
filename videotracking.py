820723d7918de8451bf0d0282b2728a166c9d72e
Russell Wakugawa <wakugawa@usc.edu>
2018-11-30 15:36:47 -0800

    opencv

---
+++ videotracking.py
@@ -0,0 +1,130 @@
+import cv2
+import sys
+from Queue import Queue
+from threading import Thread
+import time
+
+class FileVideoStream:
+    def __init__(self, path, queueSize=128):
+        # initialize the file video stream along with the boolean
+        # used to indicate if the thread should be stopped or not
+        self.stream = cv2.VideoCapture(path)
+        self.stopped = False
+        # initialize the queue used to store frames read from
+        # the video file
+        self.Q = Queue(maxsize=queueSize)
+
+    def start(self):
+        # start a thread to read frames from the file video stream
+        t = Thread(target=self.update, args=())
+        t.daemon = True
+        t.start()
+        return self
+
+    def update(self):
+        # keep looping infinitely
+        while True:
+            # if the thread indicator variable is set, stop the
+            # thread
+            if self.stopped:
+                return
+ 
+            # otherwise, ensure the queue has room in it
+            if not self.Q.full():
+                # read the next frame from the file
+                (grabbed, frame) = self.stream.read()
+ 
+                # if the `grabbed` boolean is `False`, then we have
+                # reached the end of the video file
+                if not grabbed:
+                    self.stop()
+                    return
+ 
+                # add the frame to the queue
+                self.Q.put(frame)
+
+            time.sleep(0.25)
+    
+    def read(self):
+        # return next frame in the queue
+        return self.Q.get()
+    
+    def more(self):
+        # return True if there are still frames in the queue
+        return self.Q.qsize() > 0
+
+    def stop(self):
+        # indicate that the thread should be stopped
+        self.stopped = True
+
+    def clear(self):
+        self.Q = Queue(maxsize=128)
+ 
+if __name__ == '__main__' :
+ 
+    # Set up tracker.
+    # Instead of MIL, you can also use
+ 
+    tracker_types = ['BOOSTING', 'MIL','KCF', 'TLD', 'MEDIANFLOW', 'GOTURN', 'MOSSE', 'CSRT']
+    tracker_type = tracker_types[2]
+ 
+    tracker = cv2.TrackerKCF_create()
+ 
+    # Read video
+    print("[INFO] starting video file thread...")
+    fvs = FileVideoStream(0).start()
+    time.sleep(1.0)
+ 
+    # Read first frame.
+    frame = fvs.read()
+     
+    # Define an initial bounding box
+    bbox = (287, 23, 86, 320)
+ 
+    # Uncomment the line below to select a different bounding box
+    bbox = cv2.selectROI(frame, False)
+ 
+    # Initialize tracker with first frame and bounding box
+    ok = tracker.init(frame, bbox)
+
+    fvs.clear()
+ 
+    while True:
+        # Read a new frame
+        frame = fvs.read()
+         
+        # Start timer
+        timer = cv2.getTickCount()
+ 
+        # Update tracker
+        ok, bbox = tracker.update(frame)
+ 
+        # Calculate Frames per second (FPS)
+        fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
+ 
+        # Draw bounding box
+        if ok:
+            # Tracking success
+            p1 = (int(bbox[0]), int(bbox[1]))
+            p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+            cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
+        else :
+            # Tracking failure
+            cv2.putText(frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
+ 
+        # Display tracker type on frame
+        cv2.putText(frame, tracker_type + " Tracker", (100,20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50),2)
+     
+        # Display FPS on frame
+        cv2.putText(frame, "FPS : " + str(int(fps)), (100,50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2)
+ 
+        # Display result
+        cv2.imshow("Tracking", frame)
+ 
+        # Exit if ESC pressed
+        k = cv2.waitKey(1) & 0xff
+        if k == 27 : break
+    
+    # do a bit of cleanup
+    cv2.destroyAllWindows()
+    fvs.stop()
